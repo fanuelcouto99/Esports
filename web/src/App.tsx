0d0c0c1170b3b7c5564b799d5e6127c76798459c
Fanuel Couto <fanuel.couto99@gmail.com>
2022-09-13 17:16:26 -0300

    Configurado css da landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import './styles/main.css';
 import logoImg from './assets/logo-nlw-esports.svg';
+import { MagnifyingGlassPlus } from 'phosphor-react';
 
 function App() {
   return (
@@ -60,8 +61,18 @@ function App() {
         </a>
       </div>
 
-      <div className='bg-[#2a2634] px-8 py-6 mt-8 self-stretch rounded-lg'>
+      <div className='pt-1 bg-nlw-gradient self-stretch mt-8 rounded-lg overflow-hidden'>
+        <div className='bg-[#2a2634] px-8 py-6 flex justify-between items-center'>
+          <div>
+            <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
+            <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
+          </div>
 
+          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
       </div>
     </div>
   );
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,11 +5,15 @@ module.exports = {
     './index.html'
   ],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
+
     extend: {
       colors: {
-        
+
       },
-      
+
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
         'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
