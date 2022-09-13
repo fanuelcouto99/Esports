2a135bfda0a6f55b0464f26e75bbdfb3d558a0ff
Fanuel Couto <fanuel.couto99@gmail.com>
2022-09-13 12:03:06 -0300

    Instaldo tailwind css e configurando landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,15 @@
+import './styles/main.css';
+import logoImg from './assets/logo-nlw-esports.svg';
+
 function App() {
   return (
-    <h1>Shazam Carai</h1>
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+      <img src={logoImg} alt="" />
+
+      <h1 className='text-6xl text-white font-black mt-20'>
+        Seu <span className='bg-nlw-gradient'>duo</span> está aqui.
+      </h1>
+    </div>
   );
 }
 
