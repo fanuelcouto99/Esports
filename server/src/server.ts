e28aa3ea7bba5d73fa8540ca0fd581f29c66293d
Fanuel Couto <fanuel.couto99@gmail.com>
2022-09-14 12:32:23 -0300

    Configurando banco de dados prisma para aplicação

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,21 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get('/ads', (request: Request, response: Response) => {
+app.get('/games', (request: Request, response: Response) => {
+    return response.json([]);
+});
+
+app.post('/ads', (request: Request, response: Response) => {
+    return response.status(201).json([]);
+});
+
+// Rota para pegar os ads de um game específico
+app.get('/games/:id/ads', (request: Request, response: Response) => {
+    return response.json('Acessou Ads!');
+});
+
+// Rota para pegar o discord de um anuncio específico
+app.get('/ads/:id/discord', (request: Request, response: Response) => {
     return response.json('Acessou Ads!');
 });
 
