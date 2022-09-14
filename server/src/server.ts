c75962cb1929e751afa5092eb62ef86c8de1525f
Fanuel Couto <fanuel.couto99@gmail.com>
2022-09-14 17:50:40 -0300

    Configurando rota para listagem de games

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,28 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 
-app.get('/games', (request: Request, response: Response) => {
-    return response.json([]);
+const prisma = new PrismaClient({
+    log: ['query']
 });
 
-app.post('/ads', (request: Request, response: Response) => {
+app.get('/games', async (request, response) => {
+    const games = await prisma.game.findMany();
+    return response.json(games);
+});
+
+app.post('/ads', (request, response) => {
     return response.status(201).json([]);
 });
 
 // Rota para pegar os ads de um game específico
-app.get('/games/:id/ads', (request: Request, response: Response) => {
+app.get('/games/:id/ads', (request, response) => {
     return response.json('Acessou Ads!');
 });
 
 // Rota para pegar o discord de um anuncio específico
-app.get('/ads/:id/discord', (request: Request, response: Response) => {
+app.get('/ads/:id/discord', (request, response) => {
     return response.json('Acessou Ads!');
 });
 
